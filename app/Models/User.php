a00d4a24a6238a30bba7c109aff42d8df29b5d8c
kpwhelan <60331971+kpwhelan@users.noreply.github.com>
2026-01-02 18:48:39 -0500

    Create user model, update user migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,11 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'chapter_id',
         'email',
+        'role',
         'password',
     ];
 
