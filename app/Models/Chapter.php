90c1f42d658b3abc9a3d1d5e0aed70f6637d3df2
kpwhelan <60331971+kpwhelan@users.noreply.github.com>
2026-01-02 18:54:16 -0500

    Add chapters migration

--- app/Models/Chapter.php
+++ app/Models/Chapter.php
@@ -4,7 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Chapter extends Model
-{
-    //
+class Chapter extends Model {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+    ];
 }
